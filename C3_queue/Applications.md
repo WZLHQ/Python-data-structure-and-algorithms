0fae24646158405ec71651862af460d9067c281f
QH-mac-m1 <2580054308@qq.com>
2025-12-09 22:19:35 +0800

    normal update

---
+++ C3_queue/Applications.md
@@ -0,0 +1,8 @@
+# 队列的应用
+
+## 应用1：约瑟夫问题
+
+```python
+from src.Queue import Queue
+
+```
--- test.py
+++ test.py
@@ -1,12 +1,10 @@
 from src.Stack import Stack
 from src.Queue import Queue
 
-q=Queue()
-print(q.isEmpty())
-q.enqueue(4)
-print(q.list)
-q.enqueue("dog")
-print(q.size())
-print(q.isEmpty())
-print(q.dequeue())
-print(q.size())
+def Josephus(name_list, num):
+    q=Queue()
+    for n in name_list:
+        q.enqueue(n)
+        
+
+    pass
