176592134fae7b617d7f2147a0e2b95652d295ff
Rosie <2580054308@qq.com>
2026-05-13 11:26:30 +0800

    none

--- src/DoubleLinkList.py
+++ src/DoubleLinkList.py
@@ -29,7 +29,6 @@ class DoubleLinkList:
 
 
     def add(self,item):
-        # TODO refine this code
         node = DNode(item)
         if self.__head == None:
             self.__head=node
@@ -50,28 +49,60 @@ class DoubleLinkList:
             cur.next=node
             
     def insert(self,pos,item):
-        if pos < 0:
+        if pos <= 0:
             self.add(item)
-        elif pos > 0:
+        elif pos > self.length()-1:
             self.append(item)
         else:
             count=0
             node=DNode(item)
             cur=self.__head
+
+            # 指向指定位置
             while count<pos:
                 cur=cur.next
                 count+=1
-            node.next=cur
+
             node.pre=cur.pre
+            node.next=cur
             cur.pre.next=node
             cur.pre=node
 
+            # 指向指定位置的前一个
+            # 这两种写法有问题吗
+            # while count<(pos-1):
+            #     cur=cur.next
+            #     count+=1
+            # node.pre=cur
+            # node.next=cur.next
+            # cur.next=node
+            # node.next.pre=node
+
     def remove(self,item):
-        pass
+        cur=self.__head
+        while cur != None:
+            if cur.data==item:
+                if cur==self.__head:
+                    # TODO 此处用cur=cur.next行不行？
+                    self.__head=cur.next
+                    if cur.next:
+                        cur.next.pre=None
+                elif cur.next != None:
+                    cur.pre.next=cur.next
+                    cur.next.pre=cur.pre
+                else:
+                    cur.pre.next=None
+                    cur.pre=None
+                break
+            cur=cur.next
 
     def search(self,item):
-        pass
-
+        cur=self.__head
+        while cur != None:
+            if cur.data == item:
+                return True
+            cur=cur.next
+        return False
 
 
 if __name__ =="__main__":
